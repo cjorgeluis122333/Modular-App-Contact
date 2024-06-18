1bf40b9144eee6b75443fbcd4d6cae46f619b3f6
jcvidal <jcvidal@gmail.com>
2024-06-17 23:52:52 -0400

    Home

--- feature/home/src/main/java/cu/xetid/dtvc/androidtrainingapp/home/screen/FavoriteScreen.kt
+++ feature/home/src/main/java/cu/xetid/dtvc/androidtrainingapp/home/screen/FavoriteScreen.kt
@@ -1,24 +1,15 @@
 package cu.xetid.dtvc.androidtrainingapp.home.screen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import cu.xetid.dtvc.androidtrainingapp.home.screen.component.ContactButtonBar
 import cu.xetid.dtvc.androidtrainingapp.home.viewmodel.HomeViewModel
 import cu.xetid.dtvc.androidtrainingapp.ui.component.GenericTopAppBar
-import cu.xetid.dtvc.androidtrainingapp.ui.navigation.routes.insertion.InsertionRoute
 
 @Composable
 fun FavoriteScreen(viewModel: HomeViewModel = hiltViewModel()) {
